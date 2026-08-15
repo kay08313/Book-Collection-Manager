1a37f22692e30b4a76c2c2d5f655f08b027e0b7b
kay08313 <kaychiawoon48@gmail.com>
2026-08-16 02:41:57 +0900

    first update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+
+const name = "kay"
+const pw = "asdf"
+
+function login(){
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+    const result = document.getElementById("result")
+
+    const tlusernm = username.value.toLowerCase()
+    const tluserpw = password.value.toLowerCase()
+
+    if (tlusernm === name && tluserpw === pw){
+
+        window.location.href = "page2.html";
+
+    }else{
+
+        result.textContent = "Wrong User id or Password!!!"
+
+    }
+
+}
+
+
+
+
